e5093811419a066eb3af33ab0c69c3c5bc66300d
Bhanuraghav2007 <lalitkumarraghav637@gmail.com>
2026-04-09 16:17:56 +0530

    Only main file

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.title("streamlit app")
+about_page=st.Page(
+    page ='views/about.py',
+    title='About us',
+
+)
+contact_page=st.Page(
+    page='views/contact.py',
+    title='contact us',
+)
+profile_page=st.Page(
+    page='views/profile.py',
+    title='My profile',
+    default=True,
+)
+nb=st.navigation(pages=[about_page,contact_page,profile_page])
+
